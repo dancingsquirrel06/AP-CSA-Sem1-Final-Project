f58516bf3d11503bb42a3edc868c9f2f5066d1a1
dancingsquirrel06 <118295532+dancingsquirrel06@users.noreply.github.com>
2023-12-02 23:47:47 -0800

    /

---
+++ src/main/ java/src/main/java/Card.java
@@ -0,0 +1,35 @@
+public class Card {
+    private String suit;
+    private String rank;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public int getNumericValue() {
+        switch (rank) {
+            case "Ace":
+                return 11; // Ace can be 1 or 11, but initially, we'll treat it as 11
+            case "King":
+            case "Queen":
+            case "Jack":
+                return 10;
+            default:
+                return Integer.parseInt(rank); // For cards 2-10
+        }
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
