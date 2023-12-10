613580f083aca0931497bd721ab3c8d357b695cc
dancingsquirrel06 <118295532+dancingsquirrel06@users.noreply.github.com>
2023-12-10 12:32:24 -0800

    almost done

--- src/main/ java/src/main/Main.java
+++ src/main/ java/src/main/Main.java
@@ -1,99 +1,4 @@
-// package src.main;
-// import java.util.ArrayList;
-// import java.util.Collections;
-// import java.util.Scanner;
 
-
-
-// public class Main {
-    
-//     private static final Scanner scanner = new Scanner(System.in);
-//     private static ArrayList<Player> players = new ArrayList<>();
-//     private static ArrayList<Card> deck = new ArrayList<>();
-//     private static boolean gameRunning = true;
-
-//     public static void main(String[] args) {
-//         System.out.println("Welcome to BlackJack!");
-
-//         // Initialize players
-//         System.out.print("How many players will be playing in this game?: ");
-//         int numOfPlayers = scanner.nextInt();
-//         for (int i = 0; i < numOfPlayers; i++) {
-//             System.out.print("Enter player " + (i + 1) + "'s name: ");
-//             String playerName = scanner.next();
-//             players.add(new Player(playerName));
-//         }
-
-//         while (gameRunning) {
-//             startGame();
-//         }
-//     }
-
-//     public static void makeDeck() {
-//         deck.clear();
-//         String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
-//         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-//         for (String suit : suits) {
-//             for (String rank : ranks) {
-//                 deck.add(new Card(rank, suit));
-//             }
-//         }
-//         Collections.shuffle(deck);
-//         return;
-//     }
-
-//     public static void startGame() {
-//         makeDeck();
-//         for (Player p : players) {
-//             p.addCard(deck.remove(0));
-//             p.addCard(deck.remove(0));
-//             System.out.println(p.getName() + "'s Cards: " + p.getCardList());
-//         }
-//         round();
-//         return;
-//     }
-
-//     public static void round() {
-//         for (Player p : players) {
-//             // while (true) {
-//                 System.out.println(p.getName() + "'s turn. Would you like to hit or stay?");
-//                 String input = scanner.next();
-//                 if (input.equalsIgnoreCase("hit")) {
-//                     p.addCard(deck.remove(0));
-//                     System.out.println("Cards: " + p.getCardList());
-//                     if (p.getHandValue() > 21) {
-//                         System.out.println("Bust for player " + p.getName() + "!");
-//                         players.remove(p);
-//                         if (players.size() == 0) {
-//                             end();
-//                             return;
-//                         }
-//                     } else if (p.getHandValue() == 21) {
-//                         System.out.println(p.getName() + " wins!");
-//                         end();
-//                         return;
-//                     }
-//                 }
-//             // }
-//         }
-//         round();
-//         return;
-//     }
-
-//     public static void end() {
-//         System.out.print("Would you like to play again? (yes/no): ");
-//         String input = scanner.next();
-//         if (input.equalsIgnoreCase("no")) {
-//             System.out.println("Thank you for playing!");
-//             gameRunning = false;
-//         }
-//         return;
-//         // Reset players for a new game
-//         // for (Player p : players) {
-//         //     p.resetHand();
-//         // }
-//     }
-// }
 package src.main;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -112,9 +17,10 @@ public class Main {
         // Initialize players
         System.out.print("How many players will be playing in this game?: ");
         int numOfPlayers = scanner.nextInt();
+        scanner.nextLine(); // Consume the remaining newline
         for (int i = 0; i < numOfPlayers; i++) {
             System.out.print("Enter player " + (i + 1) + "'s name: ");
-            String playerName = scanner.next();
+            String playerName = scanner.nextLine();
             players.add(new Player(playerName));
         }
 
@@ -140,71 +46,87 @@ public class Main {
 
     public static void startGame() {
         makeDeck();
+    
+        // Initialize the dealer
+        Player dealer = new Player("Dealer");
+    
+        // Deal cards to players and show their hands
         for (Player p : players) {
             p.addCard(deck.remove(0));
             p.addCard(deck.remove(0));
             System.out.println(p.getName() + "'s Cards: " + p.getCardList());
         }
-        boolean roundOver = false;
-        while (!roundOver) {
-            roundOver = round();
-        }
-        resetPlayers();
-    }
-
-    // public static boolean round() {
-    //     for (Player p : players) {
-    //         System.out.println(p.getName() + "'s turn. Would you like to hit or stay?");
-    //         String input = scanner.next();
-    //         if (input.equalsIgnoreCase("hit")) {
-    //             p.addCard(deck.remove(0));
-    //             System.out.println("Cards: " + p.getCardList());
-    //             if (p.getHandValue() > 21) {
-    //                 System.out.println("Bust for player " + p.getName() + "!");
-    //                 return true;
-    //             } else if (p.getHandValue() == 21) {
-    //                 System.out.println(p.getName() + " wins!");
-    //                 return true;
-    //             }
-    //         } else if (input.equalsIgnoreCase("stay")) {
-    //             // Implement logic for when player chooses to stay
-    //         }
-    //     }
-    //     return false;
-    // }
-    public static boolean round() {
-        boolean roundOver = true;
     
+        // Deal cards to dealer
+        dealer.addCard(deck.remove(0));
+        dealer.addCard(deck.remove(0));
+    
+        // Play rounds for players
         for (Player p : players) {
-            // Check if the player has already busted in a previous turn
-            if (p.getHandValue() > 21) {
-                continue; // Skip the turn for this player
-            }
+            playerTurn(p);
+        }
     
-            System.out.println(p.getName() + "'s turn. Would you like to hit or stay?");
+        // Dealer's turn
+        dealerTurn(dealer);
+    
+        // Determine outcome for each player
+        for (Player p : players) {
+            determineOutcome(p, dealer);
+        }
+    
+        // Reset players and dealer for the next game
+        resetPlayers();
+        dealer.resetHand();
+    }
+    
+
+    public static void playerTurn(Player player) {
+        while (true) {
+            System.out.println(player.getName() + "'s turn. Would you like to hit or stay?");
             String input = scanner.next();
-    
+
             if (input.equalsIgnoreCase("hit")) {
-                p.addCard(deck.remove(0));
-                System.out.println("Cards: " + p.getCardList());
-                if (p.getHandValue() > 21) {
-                    System.out.println("Bust for player " + p.getName() + "!");
-                    // Don't end the round immediately to allow other players to finish their turns
+                player.addCard(deck.remove(0));
+                System.out.println("Cards: " + player.getCardList());
+                if (player.getHandValue() > 21) {
+                    System.out.println("Bust for player " + player.getName() + "!");
+                    break; // Exit the loop if the player busts
                 }
-                roundOver = false; // Round continues as the player chose to hit
             } else if (input.equalsIgnoreCase("stay")) {
-                System.out.println(p.getName() + " stays.");
-                // Player chose to stay, check the next player
+                System.out.println(player.getName() + " stays.");
+                break; // Exit the loop if the player chooses to stay
             } else {
                 System.out.println("Invalid input. Please choose 'hit' or 'stay'.");
-                roundOver = false; // Invalid input, give the player another chance
             }
         }
-    
-        return roundOver; // Return true if all players stayed or busted, false otherwise
     }
-    
-    
+
+    public static void dealerTurn(Player dealer) {
+        System.out.println("Dealer's turn.");
+        while (dealer.getHandValue() < 17) {
+            dealer.addCard(deck.remove(0));
+        }
+        System.out.println("Dealer's Cards: " + dealer.getCardList());
+        if (dealer.getHandValue() > 21) {
+            System.out.println("Dealer busts!");
+        }
+    }
+
+    public static void determineOutcome(Player player, Player dealer) {
+        int playerValue = player.getHandValue();
+        int dealerValue = dealer.getHandValue();
+
+        System.out.print(player.getName() + ": ");
+        if (playerValue > 21) {
+            System.out.println("Busts. Loss.");
+        } else if (dealerValue > 21 || playerValue > dealerValue) {
+            System.out.println("Wins.");
+        } else if (playerValue == dealerValue) {
+            System.out.println("Push (tie).");
+        } else {
+            System.out.println("Loss.");
+        }
+    }
 
     public static boolean askToPlayAgain() {
         System.out.print("Would you like to play again? (yes/no): ");
@@ -217,7 +139,7 @@ public class Main {
     }
 
     public static void resetPlayers() {
-        for (Player p : players) {
+      for (Player p : players) {
             p.resetHand();
         }
     }
